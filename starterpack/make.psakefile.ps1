e70b903d8aeecd57a0664a95a061e939fb473e57
philip meholm <philip@nimtech.no>
2024-02-19 01:46:17 +0100

    added hash and zip

--- starterpack/make.psakefile.ps1
+++ starterpack/make.psakefile.ps1
@@ -17,16 +17,16 @@ task generateHash {
     $products | ForEach-Object {
         $folder = join-path $psake.build_script_dir $_.name
         $item = join-path $folder $_.script
-        $hash = Get-FileHash -Path $item -Algorithm SHA256
-        $hash|convertto-json|Out-File (join-path $psake.build_script_dir $_.name -AdditionalChildPath "hash.json")
+        $hash = Get-FileHash -Path $item -Algorithm SHA256|select -ExcludeProperty Path
+        $hash|convertto-json|Out-File (join-path $folder "hash.json") -Force
     }
 }
 
 task compress -depends generateHash {
-    $Folders | ForEach-Object {
-        $folder = $_
-        $zip = "$folder.zip"
-        $path = join-path $psake.build_script_dir $zip
+    $products | ForEach-Object {
+        $folder = $_.name
+        Write-host "generating zip for $folder in $($psake.build_script_dir)"
+        $zip = join-path $psake.build_script_dir "$folder.zip"
         if (Test-Path $zip) {
             Remove-Item $zip
         }
